8ccd93a7c6d5023ed382ff8ceed0425d92ec9733
Arnaud Gissinger <agissing@student.42.fr>
2023-08-29 18:36:06 +0200

    [FIX] shiftTime

--- src/shiftTime.ts
+++ src/shiftTime.ts
@@ -10,29 +10,29 @@ import { ParseResponse } from './parse';
  * @returns The shifted time object
  */
 export function shiftTime(parsedTime: ParseResponse, offsetMinutes: number): ParseResponse {
-    const shift = offsetMinutes + parsedTime.shiftInMinutes;
-    const utcMinutesRaw = parsedTime.minutes - (shift % 60);
-    const utcMinutesRemaining = utcMinutesRaw / 60;
+    const shift = offsetMinutes - parsedTime.shiftInMinutes;
+    const minutesRaw = parsedTime.minutes + (shift % 60);
+    const nbHours = minutesRaw / 60;
 
     let remaining = 0;
-    if (utcMinutesRemaining < 0) {
+    if (nbHours < 0) {
         remaining = -1;
-    } else if (utcMinutesRemaining > 60) {
+    } else if (nbHours > 1) {
         remaining = 1;
     }
 
-    const utcHours = (240 + parsedTime.hours - ((shift / 60) >> 0) + remaining) % 24;
-    const utcMinutes = (600 + utcMinutesRaw) % 60;
+    const hours = (240 + parsedTime.hours + ((shift / 60) >> 0) + remaining) % 24;
+    const minutes = (600 + minutesRaw) % 60;
 
     return {
         ...parsedTime,
-        hours: utcHours,
-        minutes: utcMinutes,
+        hours,
+        minutes,
         rawShift: {
-            hours: (Math.abs(shift) / 60) >> 0,
-            minutes: Math.abs(shift) % 60,
-            sign: shift >= 0 ? 1 : -1,
+            hours: (Math.abs(offsetMinutes) / 60) >> 0,
+            minutes: Math.abs(offsetMinutes) % 60,
+            sign: offsetMinutes > 0 ? -1 : 1,
         },
-        shiftInMinutes: shift,
+        shiftInMinutes: offsetMinutes,
     };
 }
